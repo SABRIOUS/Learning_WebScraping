a3bdb53865d5c6060a4e32c2456e7f27b4243355
SABRIOUS <ahmedsabrious@gmail.com>
2019-10-27 17:07:29 +0200

    tutorial_two

--- middel_html.py
+++ middel_html.py
@@ -5,8 +5,24 @@ soup = BeautifulSoup(ITEM_HTML,'html.parser')
 
 
 def find_item_name():
-    get_title = soup.find_all('h3','title')
-    return get_title
+    locator = "article.product_pod h3 a "
+    item_link = soup.select_one(locator)
+    item_name = item_link.attrs['href']
+    return item_name
 
 
-print(find_item_name())
+
+def find_item_price():
+    locator = "article.product_pod p.price_color "
+    item_price = soup.select_one(locator).string
+    return item_price[1:]
+
+
+def find_rating():
+    locator = "article.product_pod p.star-rating"
+    start_rating_tag = soup.select_one(locator)
+    calsses = start_rating_tag.attrs['class']
+    final_rate = [r for r in calsses if r != 'star-rating']
+    return final_rate[0]
+
+print(find_rating())
