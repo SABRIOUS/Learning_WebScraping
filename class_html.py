93ea01e62134c2474b6b3fee9911c3b32f573494
SABRIOUS <ahmedsabrious@gmail.com>
2019-10-27 18:29:08 +0200

    add @property

--- class_html.py
+++ class_html.py
@@ -5,23 +5,28 @@ class ParsedItem:
     def __init__(self,page):
         self.soup = BeautifulSoup(page,'html.parser')
 
-    def find_item_name(self):
+    @property
+    def name(self):
         locator = "article.product_pod h3 a "
         item_link = self.soup.select_one(locator)
         item_name = item_link.attrs['title']
         return item_name
-    def find_item_link(self):
+
+    @property
+    def link(self):
         locator = "article.product_pod h3 a "
         item_link = self.soup.select_one(locator)
         item_name = item_link.attrs['href']
         return item_name
 
-    def find_item_price(self):
+    @property
+    def price(self):
         locator = "article.product_pod p.price_color "
         item_price = self.soup.select_one(locator).string
         return item_price[1:]
 
-    def find_rating(self):
+    @property
+    def rating(self):
         locator = "article.product_pod p.star-rating"
         start_rating_tag = self.soup.select_one(locator)
         calsses = start_rating_tag.attrs['class']
@@ -29,9 +34,7 @@ class ParsedItem:
         return final_rate[0]
 
 
-item = ParsedItem(ITEM_HTML)
 
-print(item.find_rating())
-print(item.find_item_name())
-print(item.find_item_price())
-print(item.find_item_link())
+
+item = ParsedItem(ITEM_HTML)
+print(item.rating)
